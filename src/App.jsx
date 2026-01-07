78a84aea12a594375d7f08921bf2e62a8df4dc10
nancysangani <9d.24.nancy.sangani@gmail.com>
2026-01-08 00:28:40 +0530

    Add vans list and dynamic van details route

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
-import Header from "./components/Header.jsx";
+import Layout from "./components/Layout.jsx";
+import Vans from "./pages/Vans.jsx";
+import VanDetail from "./pages/VanDetail.jsx";
+import "./server";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VanDetail />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
