ce5692f51b5c98db8fc4965991e7f509dc3d1519
nancysangani <9d.24.nancy.sangani@gmail.com>
2026-01-07 14:24:24 +0530

    Added basic routing structure

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Header from "./components/Header.jsx";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
