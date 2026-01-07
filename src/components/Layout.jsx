1b2024438a77ee37d3215f194f658ad942f654b6
nancysangani <9d.24.nancy.sangani@gmail.com>
2026-01-08 00:45:02 +0530

    Add Outlet to layout for nested routing

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,11 @@
+import { Outlet } from "react-router-dom";
+import Header from "./Header.jsx";
+
 export default function Layout() {
   return (
-    <h1>Layout Component</h1>
-  )
+    <>
+      <Header />
+      <Outlet />
+    </>
+  );
 }
