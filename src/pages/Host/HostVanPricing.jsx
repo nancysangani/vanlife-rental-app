c62e084bdb48ba3c8eb34c358079fca7e274f3d6
nancysangani <9d.24.nancy.sangani@gmail.com>
2026-01-17 00:07:24 +0530

    fixed the UI issue

--- src/pages/Host/HostVanPricing.jsx
+++ src/pages/Host/HostVanPricing.jsx
@@ -3,7 +3,7 @@ export default function HostVanPricing() {
   const { currentVan } = useOutletContext();
   return (
     <h3 className="host-van-price">
-      ${currentVan.price}
+      ₹{currentVan.price}
       <span>/day</span>
     </h3>
   );
